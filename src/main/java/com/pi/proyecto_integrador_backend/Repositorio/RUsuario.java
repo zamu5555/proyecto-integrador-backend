2e571ef376369a2c6213f6e5383695be9a15e93e
Mayra Alzate <nadja.minni@gmail.com>
2026-03-12 15:40:24 -0500

    Reubicacion carpeta Repositorio

--- src/main/java/com/pi/proyecto_integrador_backend/Repositorio/RUsuario.java
+++ src/main/java/com/pi/proyecto_integrador_backend/Repositorio/RUsuario.java
@@ -1,4 +1,4 @@
-package Repositorio;
+package com.pi.proyecto_integrador_backend.Repositorio;
 
 import com.pi.proyecto_integrador_backend.Modelo.MUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
